a937dced3a5582f17da47d5ded86f6f323b82a1e
Muhammad Ridhwan <44764802+aviendre@users.noreply.github.com>
2023-03-23 03:33:01 +0800

    clean up

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
-from flask import Flask, request, jsonify, render_template
-import pickle
+from flask import Flask, request, jsonify
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -14,29 +13,31 @@ import glob
 import os
 from PIL import Image
 
-import cv2
+import cv2 
 
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import imshow
 
 
 from string import digits
+import pickle
 
-dirImage = './Train'
+dirImage = './classifier/img_classifier_model/Train'
 images_path = os.listdir(dirImage)
+
 filenames = []
-filename = []
+filename=[]
 l = []
-c = np.zeros(5, int)
+c=np.zeros(5, int)
 for i in range(5):
-    f = []
-    f += glob.glob(dirImage+"/"+images_path[i]+"/*"+".jpg")
+    f=[]
+    f+=glob.glob(dirImage+"/"+images_path[i]+"/*"+".jpg")
     l.append(images_path[i])
-    c[i] += len(f)
+    c[i]+=len(f)
     filenames += f
 #     filenames += glob.glob(dirImage+"/"+images_path[i]+"/*"+".jpg")
-    # Create_Label
-label = []
+    #Create_Label
+label=[]
 for j in range(5):
     for k in range(c[j]):
         label.append(l[j])
@@ -49,7 +50,7 @@ data = pd.DataFrame({'path': filenames, 'class': label})
 # #     ForPredict = []
 #     Cardboard = []
 #     Plastics = []
-
+    
 #     im=[]
 
 #     for i, row in data.iterrows():
@@ -78,7 +79,7 @@ data = pd.DataFrame({'path': filenames, 'class': label})
 # #     ForPredict = np.array(ForPredict)
 #     Cardboard = np.array(Cardboard)
 #     Plastics = np.array(Plastics)
-
+    
 #     all_images = np.concatenate((Metal, Glass, Paper, Cardboard, Plastics)) #, ForPredict
 #     return all_images, im
 
@@ -86,8 +87,7 @@ data = pd.DataFrame({'path': filenames, 'class': label})
 # fig = plt.figure(figsize=(100,100))
 
 # Create the training and validation sets
-train_data, val_data = train_test_split(
-    data, test_size=0.2, stratify=data['class'], random_state=42)
+train_data, val_data = train_test_split(data, test_size=0.2, stratify=data['class'], random_state=42)
 
 # Set up the image data generators
 train_datagen = ImageDataGenerator(rescale=1./255)
@@ -99,16 +99,12 @@ img_width = 64
 batch_size = 32
 
 # Create data generators
-train_generator = train_datagen.flow_from_dataframe(train_data, x_col='path', y_col='class', target_size=(
-    img_height, img_width), batch_size=batch_size, class_mode='categorical')
-val_generator = val_datagen.flow_from_dataframe(val_data, x_col='path', y_col='class', target_size=(
-    img_height, img_width), batch_size=batch_size, class_mode='categorical')
-
+train_generator = train_datagen.flow_from_dataframe(train_data, x_col='path', y_col='class', target_size=(img_height, img_width), batch_size=batch_size, class_mode='categorical')
+val_generator = val_datagen.flow_from_dataframe(val_data, x_col='path', y_col='class', target_size=(img_height, img_width), batch_size=batch_size, class_mode='categorical')
 
 def build_model():
     model = tf.keras.Sequential([
-        layers.Conv2D(32, (3, 3), activation='relu',
-                      input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
         layers.MaxPooling2D((2, 2)),
         layers.Conv2D(64, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)),
@@ -117,19 +113,17 @@ def build_model():
         layers.Dense(64, activation='relu'),
         layers.Dense(5, activation='softmax')
     ])
-
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
+
 
 
 model = build_model()
 model.summary()
 
 epochs = 10
-history = model.fit(
-    train_generator, validation_data=val_generator, epochs=epochs)
-
+history = model.fit(train_generator, validation_data=val_generator, epochs=epochs)
 
 def predict_image(model, image_path):
     img = Image.open(image_path).resize((img_width, img_height))
@@ -142,8 +136,15 @@ def predict_image(model, image_path):
     return predicted_class
 
 
+train_accuracy = history.history['accuracy'][-1]
+val_accuracy = history.history['val_accuracy'][-1]
+
+print(f"Training accuracy: {train_accuracy:.2%}")
+print(f"Validation accuracy: {val_accuracy:.2%}")
+
+
 # Test the model on a new image
-test_dir = './Test'
+test_dir = './classifier/img_classifier_model/Test'
 test_image_files = [f for f in os.listdir(test_dir) if f.endswith('.jpg')]
 for image_file in test_image_files:
     test_image_path = os.path.join(test_dir, image_file)
@@ -154,7 +155,12 @@ for image_file in test_image_files:
 predicted_class = predict_image(model, test_image_path)
 print(f"Predicted class: {predicted_class}")
 
-with open('train_generator.pkl', 'wb') as f:
-    pickle.dump(train_generator, f)
 
+with open('class_indices.pkl', 'wb') as f:
+    pickle.dump(train_generator.class_indices, f)
+
+    
 model.save('image_classifier.h5')
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-from flask import Flask, request, jsonify, render_template
-import os
-import numpy as np
+from flask import Flask, request, jsonify, send_from_directory, render_template
+from werkzeug.utils import secure_filename
 import pickle
+import os
 from PIL import Image
+import numpy as np
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+# Load the saved model
+if os.path.isfile('C:/Users/darkp/Desktop/CSC2012-Project/image_classifier.h5'):
+    try:
+        model = tf.keras.models.load_model('C:/Users/darkp/Desktop/CSC2012-Project/image_classifier.h5')
+    except OSError as e:
+        print("Error opening file:", e)
+else:
+    print("File not found")
+
+# Load the saved class indices
+with open('class_indices.pkl', 'rb') as f:
+    class_indices = pickle.load(f)
+
+# Reverse the class_indices dictionary
+classes = {v: k for k, v in class_indices.items()}
+
+img_width = 64
+img_height = 64
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './uploads'
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png'}
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file provided'}), 400
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        img = Image.open(file_path).resize((img_width, img_height))
+        img_array = np.array(img) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
+        prediction = model.predict(img_array)
+        predicted_class = classes[np.argmax(prediction)]
+
+        return jsonify({'class': predicted_class}), 200
+    else:
+        return jsonify({'error': 'Invalid file format'}), 400
 
 @app.route("/prediction", methods=['GET', 'POST'])
 def prediction():
@@ -18,15 +65,25 @@ def prediction():
         file = request.files['file']
         filename = file.filename
         uploads_dir = os.path.join(os.getcwd(), 'static\\uploads')
-        file_path = os.path.join(r'C:/Users/darkp/Desktop/CSC2012-Project/classifier/img_classifier_model/static/uploads/', filename)                       #slashes should be handeled properly
+
+        # Save image locally
+        file_path = os.path.join(r'C:/Users/darkp/Desktop/CSC2012-Project/static/uploads/', filename)
         file.save(file_path)
+
         file_path_image = "/static/uploads/" + filename
         print(uploads_dir)
 
         # run CV command here
-        product = "Fine"
+        img = Image.open(file_path).resize((img_width, img_height))
+        img_array = np.array(img) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
+        prediction = model.predict(img_array)
+        predicted_class = classes[np.argmax(prediction)]
+        product = predicted_class
         print(product)
     return render_template('predict.html', product = product, user_image = file_path_image)  
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(port=5001,debug=True)
